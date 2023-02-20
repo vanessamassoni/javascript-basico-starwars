3361513abd662b1e428ba5e6c56a3e8eff995c38
vanessa <vanemassoni@hotmail.com>
2023-02-20 16:10:35 -0300

    Alteracao

--- js/app.js
+++ js/app.js
@@ -11,3 +11,18 @@ function boasVindas(nome){
 }
 
 boasVindas('Vanessa')
+
+
+var personagens=['personagem 1', 'personagem 2', 'personagem 3', 'personagem 4']
+
+personagens = personagens.filter(function(p){
+   return p !== 'personagem 3'
+})
+
+console.log(personagens)
+
+personagens = personagens.filter(function(p){
+    return p === 'personagem 2'
+ })
+
+ console.log(personagens)
