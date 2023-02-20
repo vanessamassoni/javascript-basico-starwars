71fb43cc2ec07729d43448438bd8a8e876add090
vanessa <vanemassoni@hotmail.com>
2023-02-20 15:33:30 -0300

    Alteracao

--- js/app.js
+++ js/app.js
@@ -3,3 +3,11 @@ console.log('Java script')
 var usuario = 'Vanessa'
 
 document.getElementById('name').innerHTML=usuario
+
+
+
+function boasVindas(nome){
+   alert(nome  + ' , boas vindas')
+}
+
+boasVindas('Vanessa')
